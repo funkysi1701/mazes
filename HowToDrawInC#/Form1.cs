b55de35032276feff341d8ae2dbad648532e6052
Simon Foster <funkysi1701@gmail.com>
2016-04-21 08:51:30 +0100

    Code for producing 1x1 grid and 2x2 grid

--- HowToDrawInC#/Form1.cs
+++ HowToDrawInC#/Form1.cs
@@ -15,21 +15,55 @@ namespace Circle
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
+            //1x1 grid
+            int[][] onebyone = new int[4][] ;
+            onebyone[0] = new int[4] { 0, 0, 0, 10 };
+            onebyone[1] = new int[4] { 0, 0, 10, 0 };
+            onebyone[2] = new int[4] { 10, 0, 10, 10 };
+            onebyone[3] = new int[4] { 0, 10, 10, 10 };
+            for (int i = 0; i < onebyone.Length; i++)
+            {
+                DrawStraightLine(g, onebyone[i]);
+            }
+            //2x2
+            int[][] twobytwo = new int[16][];
+            twobytwo[0] = new int[4] { 0, 0, 0, 10 };
+            twobytwo[1] = new int[4] { 0, 0, 10, 0 };
+            twobytwo[2] = new int[4] { 10, 0, 10, 10 };
+            twobytwo[3] = new int[4] { 0, 10, 10, 10 };
 
-            DrawStraightLine(g);
+            twobytwo[4] = new int[4] { 10, 0, 10, 10 };
+            twobytwo[5] = new int[4] { 10, 0, 20, 0 };
+            twobytwo[6] = new int[4] { 20, 0, 20, 10 };
+            twobytwo[7] = new int[4] { 10, 10, 20, 10 };
 
-            DrawCircleUsingLines(g);
+            twobytwo[8] = new int[4] { 0, 10, 0, 20 };
+            twobytwo[9] = new int[4] { 0, 10, 10, 10 };
+            twobytwo[10] = new int[4] { 10, 10, 10, 20 };
+            twobytwo[11] = new int[4] { 0, 20, 10, 20 };
+
+            twobytwo[12] = new int[4] { 10, 10, 10, 20 };
+            twobytwo[13] = new int[4] { 10, 10, 20, 10 };
+            twobytwo[14] = new int[4] { 20, 10, 20, 20 };
+            twobytwo[15] = new int[4] { 10, 20, 20, 20 };
+            for (int i = 0; i < twobytwo.Length; i++)
+            {
+                DrawStraightLine(g, twobytwo[i]);
+            }
+            //3x3
+
+            //DrawCircleUsingLines(g);
         }
 
         /// <summary>
         /// Draws a single line on g
         /// </summary>
         /// <param name="g"></param>
-        private static void DrawStraightLine(Graphics g)
+        private static void DrawStraightLine(Graphics g, int[] c)
         {
             var colour = Pens.Green;
-            var startPoint = new Point(0, 0);
-            var endPoint = new Point(1000, 1000);
+            var startPoint = new Point(c[0], c[1]);
+            var endPoint = new Point(c[2], c[3]);
 
             g.DrawLine(colour, startPoint, endPoint);
         }
