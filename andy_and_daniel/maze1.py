25d698af5b7fbbfeffefd1743f426994d6ee1e06
Andrew Plummer <plummer574@gmail.com>
2016-05-11 19:20:44 +0100

    Just a grid

---
+++ andy_and_daniel/maze1.py
@@ -0,0 +1,74 @@
+
+
+
+class Cell(object):
+    def __init__(self, upper, side):
+        self.upper = upper
+        self.side = side
+
+
+def list_of_cells(n):
+    cells = []
+    for _ in range(n):
+        cells.append(Cell(True, True))
+    return cells
+
+
+def get_maze(n):
+    maze = []
+    for _ in range(n):
+        maze.append(list_of_cells(n))
+    return maze
+
+
+import pygame, sys
+from pygame.locals import *
+
+
+def draw_maze(maze):
+    # set up the window
+    surface = pygame.display.set_mode((500, 400), 0, 32)
+
+    CELL_SIZE = 20
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+    # draw the white background onto the surface
+    surface.fill(WHITE)
+
+    for y_index, row in enumerate(maze):
+        for x_index, cell in enumerate(row):
+            cell_top_left = (x_index * CELL_SIZE, y_index * CELL_SIZE)
+            if cell.upper:
+                end = (cell_top_left[0] + CELL_SIZE, cell_top_left[1])
+                pygame.draw.line(
+                    surface, BLACK, cell_top_left, end, 1)
+            if cell.side:
+                end = (cell_top_left[0], cell_top_left[1] + CELL_SIZE)
+                pygame.draw.line(
+                    surface, BLACK, cell_top_left, end, 1)
+
+    # Draw some shapes with
+    
+
+    # draw the window onto the screen
+    pygame.display.update()
+
+    # run the game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+
+def main():
+    n = 10
+    maze = get_maze(n)
+    draw_maze(maze)
+
+
+if __name__ == '__main__':
+    main()
